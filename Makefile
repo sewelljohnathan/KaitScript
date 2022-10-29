0c8a50f8593f55ed7a4a0f2f15f74639a040b34a
Johnathan <sewelljohnathan@outlook.com>
2022-10-28 22:58:37 -0400

    Add str() and auto removal of trailing 0s when printing

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ CC = gcc
 SRC_CODE = src/driver.c src/lexgen.c src/interpreter.c src/interpreterTools.c src/standard.c
 
 all: $(SRC_CODE)
-	$(CC) $(SRC_CODE) -o kait
+	$(CC) $(SRC_CODE) -lm -o kait
--- src/standard.c
+++ src/standard.c
@@ -1,8 +1,11 @@
 #include "interpreter.h"
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 
 void stdPrint();
+void stdStr();
+int removeTrailingZeroes(double num, char* stream);
 
 int checkStandards(char* name) {
 
@@ -12,6 +15,10 @@ int checkStandards(char* name) {
     }
     if (strcmp(name, "println") == 0) {
         stdPrint(1);
+        return 1;
+    }
+    if (strcmp(name, "str") == 0) {
+        stdStr();
         return 1;
     }
 
@@ -24,6 +31,7 @@ void setStandards() {
     funcParam funcParams[1];
     addFuncVar("print", funcParams, 0, 0);
     addFuncVar("println", funcParams, 0, 0);
+    addFuncVar("str", funcParams, 0, 0);
 }
 
 void stdPrint(int lineFlag) {
@@ -41,18 +49,55 @@ void stdPrint(int lineFlag) {
     lexeme rparen = nextLex();
     if (rparen.sym != rparensym) { raiseError(lparen, "No closing \")\""); }
 
+    // Remove trailing
     if (argType == numtype) {
-        if (lineFlag) {
-            printf("%lf\n", argNum);
-        } else {
-            printf("%lf", argNum);
+        removeTrailingZeroes(argNum, argText);
+    }
+
+    // Print
+    if (lineFlag) {
+        printf("%s\n", argText);
+    } else {
+        printf("%s", argText);
+    }
+
+}
+
+void stdStr() {
+
+    // Opening Paren
+    lexeme lparen = nextLex();
+    if (lparen.sym != lparensym) { raiseError(lparen, "No opening \"(\""); }
+
+    double arg = numExpression();
+    removeTrailingZeroes(arg, returnText);
+
+    // Closing Paren
+    lexeme rparen = nextLex();
+    if (rparen.sym != rparensym) { raiseError(lparen, "No closing \")\""); }
+}
+
+int removeTrailingZeroes(double num, char* stream) {
+
+    // Find how many to remove
+    int placement = -1;
+    for (int i = 6; i >= 0; i--) {
+        if (num*pow(10.0, i) - (int) (num*pow(10.0, i)) != 0) {
+            placement = i+1;
         }
     }
-    if (argType == texttype) {
-        if (lineFlag) {
-            printf("%s\n", argText);
-        } else {
-            printf("%s", argText);
-        }
+    if (placement == -1) { placement = 0; }
+
+    // Format to remove
+    switch (placement) {
+        case 0: sprintf(stream, "%.0lf", num); break;
+        case 1: sprintf(stream, "%.1lf", num); break;
+        case 2: sprintf(stream, "%.2lf", num); break;
+        case 3: sprintf(stream, "%.3lf", num); break;
+        case 4: sprintf(stream, "%.4lf", num); break;
+        case 5: sprintf(stream, "%.5lf", num); break;
+        case 6: sprintf(stream, "%.6lf", num); break;
+        case 7: sprintf(stream, "%.6lf", num); break;
     }
+
 }
