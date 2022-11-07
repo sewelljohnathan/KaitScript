8474e36b9df1a1eb792d53851931cba10f046543
Johnathan <sewelljohnathan@outlook.com>
2022-11-06 22:22:19 -0500

    Reorganize headers

--- src/interpreter.h
+++ src/interpreter.h
@@ -11,13 +11,9 @@ varType returnType;
 lexeme* lexList;
 int lexIndex;
 
+// interpreter main
+void raiseError(lexeme lex, char* msg);
 void line();
-void addNumVar(char* name, double value);
-void addTextVar(char* name, char* text);
-void addFuncVar(char* name, funcParam* funcParams, int funcParamLength, int funcStart, varType funcType);
-int findVar(char* name);
-void markVars();
-void printVarTable();
 void handleNumDeclaration();
 void handleTextDeclaration();
 void handleFuncDeclaration();
@@ -26,13 +22,20 @@ void handleFuncCall(lexeme identifier);
 void handleLoop();
 void numExpression(double* num);
 void textExpression(char* text);
+
+// interpreter tools
+int findVar(char* name);
+void markVars();
+void printVarTable();
 lexeme nextLex();
+void addNumVar(char* name, double value);
+void addTextVar(char* name, char* text);
+void addFuncVar(char* name, funcParam* funcParams, int funcParamLength, int funcStart, varType funcType);
 int isOperator(lexeme lex);
 int operatorPrecedence(lexeme lex);
 int isNegator(int index, int pastFirst);
-void raiseError(lexeme lex, char* msg);
-
-void unknownExpression(double* num, char* text, varType* expressionType);
 int convertNumToText(double num, char* stream);
+
+// standards
 void setStandards();
 int checkStandards(char* name);
